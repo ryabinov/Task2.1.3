4d2b53744896853f3dc27742f8b012764f2688bf
Dmitrii R <rojoc@yandex.ru>
2020-11-30 20:36:00 +0300

    taska

--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -3,7 +3,7 @@ package app.model;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
-import java.sql.Time;
+
 @Component
 public class AnimalsCage {
 
